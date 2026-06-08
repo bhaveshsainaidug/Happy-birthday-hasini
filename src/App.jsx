fc4e6745a9714ab25465dd7f9f822c0612915047
Bhavesh <bhaveshsainaidug@gmail.com>
2026-06-09 01:16:28 +0530

    Fix photos and remove old grid design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import confetti from 'canvas-confetti';
 import { useDevice } from './hooks/useDevice';
 import Scene1 from './scenes/Scene1';
 import Scene2 from './scenes/Scene2';
-import Scene3 from './scenes/Scene3';
 import Scene4 from './scenes/Scene4';
 import Scene5 from './scenes/Scene5';
 import Scene6 from './scenes/Scene6';
@@ -24,7 +23,7 @@ export default function App() {
   };
 
   const advanceScene = () => {
-    if (currentScene < 5) {
+    if (currentScene < 4) {
       confetti({ particleCount: 40, spread: 70, origin: { y: 1 }, zIndex: 999 });
       setCurrentScene(prev => prev + 1);
     }
@@ -36,10 +35,9 @@ export default function App() {
       {/* Render current scene */}
       {currentScene === 0 && <Scene1 onComplete={advanceScene} startMusic={startMusic} />}
       {currentScene === 1 && <Scene2 onComplete={advanceScene} />}
-      {currentScene === 2 && <Scene3 onComplete={advanceScene} device={device} />}
-      {currentScene === 3 && <Scene4 onComplete={advanceScene} />}
-      {currentScene === 4 && <Scene5 onComplete={advanceScene} device={device} />}
-      {currentScene === 5 && <Scene6 />}
+      {currentScene === 2 && <Scene4 onComplete={advanceScene} />}
+      {currentScene === 3 && <Scene5 onComplete={advanceScene} device={device} />}
+      {currentScene === 4 && <Scene6 />}
     </div>
   );
 }
